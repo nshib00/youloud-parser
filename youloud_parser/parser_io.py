3b6ee20529d47c49344bcd2c5a28f933a32fe660
nshib <nshib54321@gmail.com>
2024-08-30 10:37:35 +0300

    Add colors in output messages

--- youloud_parser/parser_io.py
+++ youloud_parser/parser_io.py
@@ -9,18 +9,14 @@ console = Console()
 
 
 def get_album_query() -> str:
-    return input('Введите запрос (исполнитель и/или название альбома): ')
-
+    return console.input('[#4be38f]Введите запрос[/] [i #777777](исполнитель и/или название альбома): [/]')
+ 
 
 async def choose_albums_to_download(albums: list[Album]) -> list[Album]:
-    console.print('Выберите альбом(ы), которые необходимо скачать:')
-    console.print(
-        '[#ff7800]* ВНИМАНИЕ: за раз можно скачать не более 3 альбомов (ограничение сайта).\n\n[/]'
-    )
     albums_checkbox = [
             inquirer.Checkbox(
             'albums',
-            message='Выберите альбомы',
+            message='Выберите альбом(ы), которые хотите скачать',
             choices=albums,
             carousel=True,
         )
@@ -28,37 +24,42 @@ async def choose_albums_to_download(albums: list[Album]) -> list[Album]:
     try:
         needed_albums: dict = inquirer.prompt(albums_checkbox)
         return needed_albums.get('albums')
-    except IndexError:
+    except (AttributeError, IndexError):
+        console.print('[#f5bc42]Альбомов по запросу не найдено. Попробуйте немного изменить предыдущий запрос или ввести новый.[/]')
         return []
     
 
 def print_album_data_message(album: Album) -> None:
-    print(f'Получаю данные об альбоме "{album.artist} - {album.title}"...')
+    console.print(f'[#a0dddd]Получаю данные об альбоме [i]"{album.artist} - {album.title}"...[/][/]')
     
 
-def print_download_message(album: Album) -> None:
-    print(f'Скачиваю альбом "{album.artist} - {album.title}"...')
+def print_download_message() -> None:
+    console.print('[#a0dddd]Скачиваю альбом...[/]')
 
 
-def print_album_code_message(album: Album, response_json: dict) -> None:
+def print_album_code_message(response_json: dict) -> None:
     if response_json['status'] == True:
-        print_download_message(album)
+        print_download_message()
     else:
         if response_json['code'] == -2: 
             timeleft = response_json['timeleft']
             if '1 часов' in timeleft:
                 timeleft = timeleft.replace('часов', 'час')
-            print(f'Достигнут лимит скачивания альбомов. До обнуления лимита осталось: {timeleft}.')
+            console.print(f'[orangered]Достигнут лимит скачивания альбомов. До обнуления лимита осталось:[/] [b green]{timeleft}.[/]')
         else:
-            print(f'Возникла ошибка. Код ошибки: {response_json["code"]}.')
-            print('Ответ сервера: ', response_json)
+            console.print(f'[firebrick]Возникла ошибка. Код ошибки: {response_json["code"]}.[/]')
+            console.print(f'[maroon]Ответ сервера: {response_json}[/]')
 
 
 def print_message_after_download(album: Album) -> None:
-    print(f'Альбом "{album.artist} - {album.title}" скачан успешно.')
+    console.print(f'[#4be38f]Альбом [i]"{album.artist} - {album.title}"[/] скачан успешно.[/]')
 
 
 def print_save_album_message(album_path: Path) -> None:
-    print(f'Путь до папки с альбомом: {album_path}.')
+    console.print(f'[white]Путь до папки с альбомом:[/] [i #a0c2c2]{album_path}.[/]')
+
+
+def print_albums_not_found_message() -> None:
+    console.print('[#f5bc42]Альбомов по запросу не найдено.[/]')
 
 
